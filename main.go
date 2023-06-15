2e142fb77521fb2a5480e40e484ff6e43f049a81
Alejandro González <alejandro.gonzalez@adevinta.com>
2023-06-15 19:00:28 +0200

    Code refinements: handlers in function

--- main.go
+++ main.go
@@ -128,6 +128,126 @@ func sendRoomToClients(roomID string, userName string) {
 	}
 }
 
+func wsHandler(c *gin.Context) {
+	roomID := c.Param("room")
+
+	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		fmt.Println("Error al actualizar a WebSocket:", err)
+		return
+	}
+
+	xUsername := c.Query("username")
+	userName := ""
+	if xUsername != "" {
+		userName = xUsername
+	} else {
+		userName = generateFunnyUsername()
+	}
+
+	connections, exists := roomClients[roomID]
+	if !exists {
+		connections = make(map[*websocket.Conn]string)
+		roomClients[roomID] = connections
+	}
+
+	connections[conn] = userName
+
+	if !exists {
+		createRoom(roomID, userName)
+	} else {
+		joinRoom(roomID, userName)
+	}
+
+	sendRoomToClients(roomID, userName)
+
+	for {
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println("Error al leer el mensaje:", err)
+			break
+		}
+
+		var action Action
+		err = json.Unmarshal(msg, &action)
+		if err != nil {
+			fmt.Println("Error al decodificar el mensaje:", err)
+			break
+		}
+
+		switch action.Action {
+		case "vote":
+			voteValue := action.Value
+
+			room := rooms[roomID]
+			rooms.UpdateUpdatedAt(roomID)
+			for i, participant := range room.Participants {
+				if participant.UserName == userName {
+					room.Participants[i].TempVote = voteValue
+					room.Participants[i].Vote = "✋"
+					break
+				}
+			}
+
+			sendRoomToClients(roomID, "")
+
+		case "showVotes":
+			room := rooms[roomID]
+			rooms.UpdateUpdatedAt(roomID)
+			for i := range room.Participants {
+				room.Participants[i].Vote = room.Participants[i].TempVote
+			}
+			sendRoomToClients(roomID, "")
+
+		case "cleanVotes":
+			room := rooms[roomID]
+			rooms.UpdateUpdatedAt(roomID)
+			for i := range room.Participants {
+				room.Participants[i].Vote = ""
+				room.Participants[i].TempVote = ""
+			}
+			sendRoomToClients(roomID, "")
+
+		case "editUsername":
+			room := rooms[roomID]
+			rooms.UpdateUpdatedAt(roomID)
+			for i := range room.Participants {
+				if room.Participants[i].UserName == userName {
+					room.Participants[i].UserName = action.Value
+					userName = action.Value
+					sendRoomToClients(roomID, action.Value)
+					break
+				}
+			}
+		}
+	}
+
+	delete(connections, conn)
+	leaveRoom(roomID, userName)
+
+	sendRoomToClients(roomID, "")
+
+	err = conn.Close()
+	if err != nil {
+		fmt.Println("Error al cerrar la conexión WebSocket:", err)
+	}
+}
+
+func roomHandler(c *gin.Context) {
+	roomId := c.Params.ByName("room")
+	rooms.UpdateUpdatedAt(roomId)
+	room, ok := rooms[roomId]
+	if !ok {
+		now := time.Now()
+		rooms[roomId] = Room{CreatedAt: now, UpdatedAt: now, ID: roomId}
+		room = rooms[roomId]
+	}
+
+	c.HTML(http.StatusOK, "room.html", gin.H{
+		"room": room,
+	})
+}
+
 func main() {
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
@@ -142,125 +262,9 @@ func main() {
 		}
 	}()
 
-	router.GET("/:room/ws", func(c *gin.Context) {
-		roomID := c.Param("room")
+	router.GET("/:room/ws", wsHandler)
 
-		conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
-		if err != nil {
-			fmt.Println("Error al actualizar a WebSocket:", err)
-			return
-		}
-
-		xUsername := c.Query("username")
-		userName := ""
-		if xUsername != "" {
-			userName = xUsername
-		} else {
-			userName = generateFunnyUsername()
-		}
-
-		connections, exists := roomClients[roomID]
-		if !exists {
-			connections = make(map[*websocket.Conn]string)
-			roomClients[roomID] = connections
-		}
-
-		connections[conn] = userName
-
-		if !exists {
-			createRoom(roomID, userName)
-		} else {
-			joinRoom(roomID, userName)
-		}
-
-		sendRoomToClients(roomID, userName)
-
-		for {
-			_, msg, err := conn.ReadMessage()
-			if err != nil {
-				fmt.Println("Error al leer el mensaje:", err)
-				break
-			}
-
-			var action Action
-			err = json.Unmarshal(msg, &action)
-			if err != nil {
-				fmt.Println("Error al decodificar el mensaje:", err)
-				break
-			}
-
-			switch action.Action {
-			case "vote":
-				voteValue := action.Value
-
-				room := rooms[roomID]
-				rooms.UpdateUpdatedAt(roomID)
-				for i, participant := range room.Participants {
-					if participant.UserName == userName {
-						room.Participants[i].TempVote = voteValue
-						room.Participants[i].Vote = "✋"
-						break
-					}
-				}
-
-				sendRoomToClients(roomID, "")
-
-			case "showVotes":
-				room := rooms[roomID]
-				rooms.UpdateUpdatedAt(roomID)
-				for i := range room.Participants {
-					room.Participants[i].Vote = room.Participants[i].TempVote
-				}
-				sendRoomToClients(roomID, "")
-
-			case "cleanVotes":
-				room := rooms[roomID]
-				rooms.UpdateUpdatedAt(roomID)
-				for i := range room.Participants {
-					room.Participants[i].Vote = ""
-					room.Participants[i].TempVote = ""
-				}
-				sendRoomToClients(roomID, "")
-
-			case "editUsername":
-				room := rooms[roomID]
-				rooms.UpdateUpdatedAt(roomID)
-				for i := range room.Participants {
-					if room.Participants[i].UserName == userName {
-						room.Participants[i].UserName = action.Value
-						userName = action.Value
-						sendRoomToClients(roomID, action.Value)
-						break
-					}
-				}
-			}
-		}
-
-		delete(connections, conn)
-		leaveRoom(roomID, userName)
-
-		sendRoomToClients(roomID, "")
-
-		err = conn.Close()
-		if err != nil {
-			fmt.Println("Error al cerrar la conexión WebSocket:", err)
-		}
-	})
-
-	router.GET("/:room", func(c *gin.Context) {
-		roomId := c.Params.ByName("room")
-		rooms.UpdateUpdatedAt(roomId)
-		room, ok := rooms[roomId]
-		if !ok {
-			now := time.Now()
-			rooms[roomId] = Room{CreatedAt: now, UpdatedAt: now, ID: roomId}
-			room = rooms[roomId]
-		}
-
-		c.HTML(http.StatusOK, "room.html", gin.H{
-			"room": room,
-		})
-	})
+	router.GET("/:room", roomHandler)
 
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil)
